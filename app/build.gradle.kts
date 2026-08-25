3c1df9fbd8e82b89e0591abb2a29d363f9176978
ahmetbysoy <ahmetbysoy@users.noreply.github.com>
2026-08-25 19:37:08 +0000

    Remove deprecated okhttp-ws dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,7 +83,6 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
-    implementation("com.squareup.okhttp3:okhttp-ws:3.14.9")
     
     // JSON
     implementation("com.squareup.moshi:moshi:1.15.0")
